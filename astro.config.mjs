ca8b9ece20686efa77839da1c039e231024d003a
VickyCliffJapardhana <falldszv23@gmail.com>
2026-02-10 15:44:25 +0800

    fix site config for github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,13 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-import github from '@astrojs/github-pages';
 
 
 // https://astro.build/config
 export default defineConfig({
 	site: 'https://vickycliffjapardhana.github.io',
   base: '/',
-	integrations: [mdx(), sitemap(), github()],
+  output: 'static',
+	integrations: [mdx(), sitemap(),],
 	
 });
