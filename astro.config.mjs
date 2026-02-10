8be10ae8215a98c382e32839d8790904b8a57ec7
VickyCliffJapardhana <falldszv23@gmail.com>
2026-02-10 15:40:50 +0800

    setup github pages deploy

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,12 +3,13 @@
 import mdx from '@astrojs/mdx';
 import sitemap from '@astrojs/sitemap';
 import { defineConfig } from 'astro/config';
-
+import github from '@astrojs/github-pages';
 
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://fal.github.io',
-	integrations: [mdx(), sitemap()],
+	site: 'https://vickycliffjapardhana.github.io',
+  base: '/',
+	integrations: [mdx(), sitemap(), github()],
 	
 });
